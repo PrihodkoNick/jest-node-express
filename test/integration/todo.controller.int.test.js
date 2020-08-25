6b1e439dfd7cc35cc0d12192692af0694e4f0455
prikhodko-fusion <nikolay.prikhodko@fusion-team.com>
2020-08-25 23:33:36 +0300

    start int tests

--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -5,10 +5,13 @@ const newTodo = require('../mock-data/new-todo.json');
 const endpointUrl = '/todos/';
 
 describe(endpointUrl, async () => {
-  it('POST ' + endpointUrl, () => {
-    request(app)
+  it('POST ' + endpointUrl, async () => {
+    const response = await request(app)
       .post(endpointUrl)
       .send(newTodo);
+      expect(response.statusCode).toBe(201);
+      expect(response.title).toBe(newTodo.title);
+      expect(response.body.done).toBe(newTodo.done);
   })
 })
 
